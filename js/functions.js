a150acbf043876d2a21d1c228fb5ee3fc4cf2d86
Hector <hectoruch1@hotmail.com>
2015-09-02 16:41:06 +0200

    new css
    new css

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+$(window).resize(function () {
+	$("#image-header").css('height', $(window).height() - 60 + "px");
+});
+$(document).ready(function () {
+	$("#image-header").css('height', $(window).height() - 60 + "px");
+});
