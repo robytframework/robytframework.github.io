2bf59cf145412794074b336bb23d4800efb2bbd7
Hector <hectoruch1@hotmail.com>
2015-09-07 19:50:08 +0200

    .
    .

--- js/functions.js
+++
@@ -1,7 +0,0 @@
-function discall() {
-	$(".textintro h1").addClass("fadeOutRight");
-	$(".textintro p").addClass("fadeOutLeft");
-	setTimeout(function () {
-		$("#nonenext").css("display", "none");
-	}, 1100);
-}
