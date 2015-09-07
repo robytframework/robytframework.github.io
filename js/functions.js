4a43dbd1e344039866ec0695d79366879560b03c
Hector <hectoruch1@hotmail.com>
2015-09-07 19:49:05 +0200

    .
    .

--- js/functions.js
+++ js/functions.js
@@ -4,27 +4,4 @@ function discall() {
 	setTimeout(function () {
 		$("#nonenext").css("display", "none");
 	}, 1100);
-	setTimeout(function () {
-		$(".thingsframe h2").css("display", "block");
-		$(".thingsframe h2").addClass("fadeIn");
-	}, 1400);
-	setTimeout(function () {
-		$("#arti1").css("display", "block");
-		$("#arti1").addClass("fadeIn");
-
-		$("#arti2").css("display", "block");
-		$("#arti2").addClass("fadeIn");
-
-		$("#arti3").css("display", "block");
-		$("#arti3").addClass("fadeIn");
-
-		$("#arti4").css("display", "block");
-		$("#arti4").addClass("fadeIn");
-
-		$("#arti5").css("display", "block");
-		$("#arti5").addClass("fadeIn");
-
-		$("#arti6").css("display", "block");
-		$("#arti6").addClass("fadeIn");
-	}, 1800);
 }
